7ea99c2e0bd171f7929ed34fc48a5704353344d9
rahatkabir007 <rahatkabir04@gmail.com>
2021-10-18 12:42:52 +0600

    nav done

---
+++ src/components/Pages/Home/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import './Header.css';
+import logo from '../../../../images/logo.png';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect sticky="top" expand="lg" className="navbar">
+                <Container>
+                    <div className="d-flex">
+                        <Navbar.Brand href="#home"><img className='logo-img' src={logo} alt="" /></Navbar.Brand>
+                        <Navbar.Toggle/>
+                   </div>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav className='nav'>
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#aboutus">About Us</Nav.Link>
+                            <Nav.Link href="#services">Services</Nav.Link>
+                            <Nav.Link href="#blog">Blog</Nav.Link>
+                            <Nav.Link href="#gallery" >Gallery</Nav.Link>
+                            <Nav.Link href="#contact" id="contact">Contact</Nav.Link>
+                            <Nav.Link href="#signup" >Sign Up</Nav.Link>
+                            <Nav.Link href="#signin">Sign In</Nav.Link>
+                        </Nav>
+                        {/* <Navbar.Text>
+                            Signed in as: <a href="#login">Mark Otto</a>
+                        </Navbar.Text> */}
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
