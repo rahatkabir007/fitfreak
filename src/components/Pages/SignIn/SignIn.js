923a5df73576e5b7e217d2ed50e5a8339641b050
rahatkabir007 <rahatkabir04@gmail.com>
2021-10-19 13:57:13 +0600

    full website ui done

--- src/components/Pages/SignIn/SignIn.js
+++ src/components/Pages/SignIn/SignIn.js
@@ -2,8 +2,39 @@ import React from 'react';
 import './SignIn.css';
 const SignIn = () => {
     return (
-        <div className="signin-section">
-            <h1>This is signin</h1>
+        <div>
+            <div className="signin-section">
+                <h1>Sign In</h1>
+            </div>
+            <div className="container my-3">
+                <div className="signin-form p-5">
+                    <form className="w-50 ms-5 my-auto signinform-area">
+                        <div className="row ">
+                            <label for="inputEmail3" className="col-sm-2 col-form-label-lg">Email</label>
+                            <div className="col-sm-12">
+                                <input type="email" className="form-control" id="inputEmail3" />
+                            </div>
+                        </div>
+                        <div className="row mb-3">
+                            <label for="inputPassword3" className="col-sm-2 col-form-label-lg">Password</label>
+                            <div className="col-sm-12">
+                                <input type="password" className="form-control" id="inputPassword3" />
+                            </div>
+                        </div>
+                        {/* <div className="row mb-3">
+                            <div className="col-sm-10 ">
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" id="gridCheck1" />
+                                    <label class="form-check-label" for="gridCheck1">
+                                        Example checkbox
+                                    </label>
+                                </div>
+                            </div>
+                        </div> */}
+                        <button type="submit" className="btn primary-btn  px-2 py-1 fs-5 ">Sign in</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Pages/Signup/Signup.js
+++ src/components/Pages/Signup/Signup.js
@@ -4,7 +4,50 @@ import './Signup.css';
 const Signup = () => {
     return (
         <div>
-            
+            <div className="signup-section">
+                <h1>Sign Up</h1>
+            </div>
+            <div className="container my-3">
+                <div className="signup-form p-5">
+                    <form className="w-50 ms-5 my-auto signupform-area">
+                        <div className="row ">
+                            <label className="col-sm-2 col-form-label-lg" for="specificSizeInputName">Name</label>
+                            <div className="col-sm-12">
+                                <input type="text" class="form-control" id="specificSizeInputName"/>
+                            </div>
+                        </div>
+                        <div className="row ">
+                            <label for="inputEmail3" className="col-sm-2 col-form-label-lg">Email</label>
+                            <div className="col-sm-12">
+                                <input type="email" className="form-control" id="inputEmail3"/>
+                            </div>
+                        </div>
+                        <div className="row mb-3">
+                            <label for="inputPassword3" className="col-sm-2 col-form-label-lg">Password</label>
+                            <div className="col-sm-12">
+                                <input type="password" className="form-control" id="inputPassword3"/>
+                            </div>
+                        </div>
+                        <div className="row mb-3">
+                            <label for="inputPassword3" className="col-sm-5 col-form-label-lg">Re-type Password</label>
+                            <div className="col-sm-12">
+                                <input type="password" className="form-control" id="inputPassword3" />
+                            </div>
+                        </div>
+                        {/* <div className="row mb-3">
+                            <div className="col-sm-10 ">
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" id="gridCheck1"/>
+                                    <label class ="form-check-label" for="gridCheck1">
+                                    Example checkbox
+                                    </label>
+                                </div>
+                            </div>
+                        </div> */}
+                        <button type="submit" className="btn primary-btn px-2 py-1 fs-5 ">Sign Up</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
